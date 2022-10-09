e890c0884c2e610d96d06d33cd9d9d5cc795d869
chapristi <70172942+chapristi@users.noreply.github.com>
2022-10-09 14:54:20 +0200

    Create index.js

---
+++ temperature-sensor/client/index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require('fs').promises;
+const host = '0.0.0.0';
+const port = 8000;
+
+const requestListener = function (req, res) {
+
+
+ fs.readFile(__dirname + "/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+
+
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
